fc1e52dda03915bcf7abbc1ef6d5546913090885
ayushgautam2196-source <ayushgautam2196@gmail.com>
2025-10-16 19:19:43 +0530

    Addition of Comment

--- src/test/java/Locators/Locators2.java
+++ src/test/java/Locators/Locators2.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-
+//a
 public class Locators2 {
 
 	public static void main(String[] args) throws InterruptedException {
